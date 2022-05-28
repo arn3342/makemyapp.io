2604a8b8ca5d99b0b564f9c574ec921787d03683
arn3342 <nabilrashid44@gmail.com>
2022-05-28 12:15:08 -0400

    Added logics

--- src/misc/logics/index.js
+++ src/misc/logics/index.js
@@ -8,20 +8,66 @@ export function filterData (source = [], query = '', ingoreProps = []) {
           const hasInitialMatch = String(data[prop])
             .toLowerCase()
             .includes(String(query).toLowerCase())
-          hasInitialMatch && !filteredList.some(x => x === data) && filteredList.push(data)
+          hasInitialMatch &&
+            !filteredList.some(x => x === data) &&
+            filteredList.push(data)
 
           const hasSubProps =
-            typeof data[prop] === 'object' && Array.isArray(Object.keys(data[prop]))
+            typeof data[prop] === 'object' &&
+            Array.isArray(Object.keys(data[prop]))
           hasSubProps &&
             Object.keys(data[prop]).map(key => {
               const hasMatch = String(data[prop][key])
                 .toLowerCase()
                 .includes(String(query).toLowerCase())
-              hasMatch && !filteredList.some(x => x === data) && filteredList.push(data)
+              hasMatch &&
+                !filteredList.some(x => x === data) &&
+                filteredList.push(data)
             })
         }
       })
     })
   }
   return query.length > 0 ? filteredList : []
+}
+
+export function getRandomItems (arr, n) {
+  var result = new Array(n),
+    len = arr.length,
+    taken = new Array(len)
+  if (n > len)
+    throw new RangeError('getRandom: more elements taken than available')
+  while (n--) {
+    var x = Math.floor(Math.random() * len)
+    result[n] = arr[x in taken ? taken[x] : x]
+    taken[x] = --len in taken ? taken[len] : len
+  }
+  return result
+}
+
+export function getNumberKMBT (n) {
+  if (n < 1e3) return n
+  if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + 'K'
+  if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + 'M'
+  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + 'B'
+  if (n >= 1e12) return +(n / 1e12).toFixed(1) + 'T'
+  return 0
+}
+
+export function getUrlExt (url, getIfImageOrVideo) {
+  // console.log('Url is:', url)
+  const type = url
+    .split(/[#?]/)[0]
+    .split('.')
+    .pop()
+    .trim()
+  if (getIfImageOrVideo) {
+    if (type.match(/gif|bmp|jpg|jpeg|png|svg|apng/g)) return 'image'
+    else return 'other'
+  }
+  return type
+}
+
+export function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
 }
